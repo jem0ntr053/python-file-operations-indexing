0c82fcabceeea0894c8620322e1e37389f69e26c
Jonathan Montrose <64240200+jem0ntr053@users.noreply.github.com>
2022-10-13 15:53:41 -0500

    created the fixed length records and added the delet function.

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ class Employee:
 
 # store the employee information in a fixed-length record file.
 def add():
-    eid = input("Enter Employee ID: ").ljust(5)
-    name = input("Enter Name: ").ljust(20)
-    age = input("Enter Age: ").ljust(2)
-    salary = input("Enter Salary: ").split().ljust(10)
-    rating = input("Enter Rating: ").split().ljust(2)
+    eid = input("Enter Employee ID: ").ljust(5)[:5]
+    name = input("Enter Name: ").ljust(20)[:20]
+    age = input("Enter Age: ").ljust(2)[:2]
+    salary = input("Enter Salary: ").ljust(7)[:7]
+    rating = input("Enter Rating: ").ljust(2)[:2]
     Employee(name, age, salary, rating)
-    # write the employee information to the file
+
+    # write the employee information to the file in fixed-length record format
     with open("employee.txt", "a") as f:
         f.write(
             eid + " " + name + " " + age + " " + salary + " " + rating + "\n")
@@ -52,18 +53,62 @@ def search():
             print("Not Found")
 
 
+# implement delete() to delete an employee record from the employee file using the index file
+def delete():
+    eid = input("Enter Employee ID: ")
+    index = []
+    with open("index.txt", "r") as f:
+        for line in f:
+            if eid == line.split()[0]:
+                print("Found")
+                rrn = line.split()[1]
+                print("RRN: ", rrn)
+                with open("employee.txt", "r") as f:
+                    for line in f:
+                        if line.split()[0] == eid:
+                            print("Employee ID: ", line.split()[0])
+                            print("Name: ", line.split()[1])
+                            print("Age: ", line.split()[2])
+                            print("Salary: ", line.split()[3])
+                            print("Rating: ", line.split()[4])
+                            break
+                break
+        else:
+            print("Not Found")
+
+    with open("index.txt", "r") as f:
+        for line in f:
+            if eid != line.split()[0]:
+                index.append(line)
+    with open("index.txt", "w") as f:
+        for line in index:
+            f.write(line)
+
+    employee = []
+    with open("employee.txt", "r") as f:
+        for line in f:
+            if eid != line.split()[0]:
+                employee.append(line)
+    with open("employee.txt", "w") as f:
+        for line in employee:
+            f.write(line)
+
+
 if __name__ == "__main__":
     print("1. Add Employee")
     print("2. Search Employee")
-    print("3. Exit")
+    print("3. Delete Employee")
+    print("4. Exit")
     choice = ""
-    while choice != "3":
+    while choice != "4":
         choice = input("Enter your choice: ")
         if choice == "1":
             add()
         elif choice == "2":
             search()
         elif choice == "3":
+            delete()
+        elif choice == "4":
             print("Exit")
         else:
             print("Invalid choice")
