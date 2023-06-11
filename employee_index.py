c141afd5858987ba43ebf38aeb4059bf040be464
Jonathan Montrose <64240200+jem0ntr053@users.noreply.github.com>
2023-06-10 20:09:23 -0500

    updated emails

--- employee_index.py
+++ employee_index.py
@@ -1,7 +1,7 @@
 __author__ = "Jonathan Montrose"
 __version__ = "1.0.1"
 __maintainer__ = "Jonathan Montrose"
-__email__ = "jem118@shsu.edu"
+__email__ = "j.e.montrose@gmail.com"
 __github_repository__ = "https://github.com/jem0ntr053/ISDesign-A03-index/tree/main"
 
 # This program will allow the user to add, search, and delete employee records.
